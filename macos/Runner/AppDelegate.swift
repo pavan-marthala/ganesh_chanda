4eb4cb95b3c361f5fcfe5b62695c0679b09256d4
pavankalyanreddy <pavan.marthala@thejatech.com>
2026-08-04 12:09:25 +0530

    impl FCM

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,8 +1,16 @@
 import Cocoa
 import FlutterMacOS
+import UserNotifications
 
 @main
 class AppDelegate: FlutterAppDelegate {
+  override func applicationDidFinishLaunching(_ notification: Notification) {
+    if #available(macOS 10.14, *) {
+      UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
+    }
+    super.applicationDidFinishLaunching(notification)
+  }
+
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
